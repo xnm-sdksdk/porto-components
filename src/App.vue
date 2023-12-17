8d7ae105ea73c19369d5a2e9663fb2c9b57259c5
xnm-sdksdk <n.mendonca@protonmail.com>
2023-12-17 00:12:34 +0000

    import router

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <NavBar></NavBar>
   <AppFooter></AppFooter>
+  <router-view></router-view>
 </template>
 
 <script>
 import NavBar from "@/components/NavBar/NavBar.vue";
 import AppFooter from "@/components/Footer/AppFooter.vue";
+import { RouterView } from "vue-router";
 export default {
   components: {
     NavBar,
