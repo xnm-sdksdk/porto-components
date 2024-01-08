a991ff024cbdd89193c93ab9422257663d9f1f81
xnm-sdksdk <n.mendonca@protonmail.com>
2024-01-08 19:48:51 +0000

    adding vuetify

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import { createApp } from "vue";
 import "./tailwind.css";
 import App from "./App.vue";
+import router from "./router";
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+app.use(vuetify);
+app.use(router);
+app.mount("#app");
