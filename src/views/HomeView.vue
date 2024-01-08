2ef35cb7142a3d53de214a93a57ccbf06192471a
xnm-sdksdk <n.mendonca@protonmail.com>
2024-01-08 19:49:08 +0000

    home page

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container fluid>
+    <v-row justify="center">
+      <v-col cols="12" md="8">
+        <v-card>
+          <v-card-title class="headline">
+            Welcome to My Vuetify App
+          </v-card-title>
+          <v-card-subtitle
+            >Building beautiful UIs with Vue.js and Vuetify</v-card-subtitle
+          >
+
+          <v-card-text>
+            <p>
+              This is a simple example of a home page using Vue.js and Vuetify.
+            </p>
+            <p>
+              Customize this page to suit your needs and start building awesome
+              applications!
+            </p>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="primary" >Learn More</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
