b1458bcec0c9c1530d43ce2520e486e1eedfb04d
xnm-sdksdk <n.mendonca@protonmail.com>
2024-01-08 19:48:58 +0000

    adding route home

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,34 @@
 import { createRouter, createWebHistory } from "vue-router";
-import AboutView from "@/views/AboutView.vue"
+import HomeView from "@/views/HomeView.vue";
+import AboutView from "@/views/AboutView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "",
-      name: "",
-      // component: ,
-      meta: {
-        title: "",
-      },
+      path: "/",
+      name: "home",
+      component: HomeView,
+      // meta: {
+      //   title: "Home",
+      // },
     },
     {
       path: "/about",
       name: "about",
-      component: AboutView ,
-      meta: {
-        title: "About",
-      },
+      component: AboutView,
+      // meta: {
+      //   title: "About",
+      // },
     },
+    {
+      path: "/services",
+      name: "services",
+      // component: ServicesView,
+      // meta: {
+      //   title: "Services",
+      // },
+    }
   ],
 });
 
