6be9c2d373cd3cd62866571c882f1d82dfefee65
xnm-sdksdk <n.mendonca@protonmail.com>
2024-01-24 14:49:06 +0000

    dummy content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,41 @@
 <template>
-  <v-container fluid>
-    <v-row justify="center">
-      <v-col cols="12" md="8">
-        <v-card>
-          <v-card-title class="headline">
-            Welcome to My Vuetify App
-          </v-card-title>
-          <v-card-subtitle
-            >Building beautiful UIs with Vue.js and Vuetify</v-card-subtitle
-          >
+  <v-app>
+    <NavBar></NavBar>
+    <v-container fluid>
+      <v-main>
+        <v-row justify="center">
+          <v-col cols="12" md="8">
+            <v-card>
+              <v-card-title class="headline">
+                Welcome to Porto Components App
+              </v-card-title>
+              <v-card-subtitle
+                >Built with Vuetify, Tailwind and Vue!</v-card-subtitle
+              >
 
-          <v-card-text>
-            <p>
-              This is a simple example of a home page using Vue.js and Vuetify.
-            </p>
-            <p>
-              Customize this page to suit your needs and start building awesome
-              applications!
-            </p>
-          </v-card-text>
-
-          <v-card-actions>
-            <v-btn color="primary" >Learn More</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+              <v-card-text>
+                <p>This is a simple example of a home page.</p>
+                <p>
+                  Customize this page to suit your needs and start building
+                  awesome applications!
+                </p>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-main>
+    </v-container>
+    <AppFooter></AppFooter>
+  </v-app>
 </template>
+
+<script>
+import NavBar from "@/components/NavBar/NavBar.vue";
+import AppFooter from "@/components/Footer/AppFooter.vue";
+export default {
+  components: {
+    NavBar,
+    AppFooter,
+  },
+};
+</script>
