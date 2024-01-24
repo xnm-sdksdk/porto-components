6e2194773b786317040a341783ee6786647e00fc
xnm-sdksdk <n.mendonca@protonmail.com>
2024-01-24 14:30:31 +0000

    footer redesign

--- src/components/Footer/AppFooter.vue
+++ src/components/Footer/AppFooter.vue
@@ -1,19 +1,35 @@
 <template>
   <footer class="bg-gray-300">
-    <div class="">
-      <h3>Get in touch</h3>
-      <p>Email: Email@email.com</p>
-      <p>Phone: (+xxx) 1234-5678-9876</p>
-    </div>
-
     <div>
-      <h3>Access Links</h3>
-      <ul>
-        <li><router-link to="/" class="p-3 m-3">Home</router-link></li>
-        <li><router-link to="/" class="p-3 m-3">About Us</router-link></li>
-        <li><router-link to="/" class="p-3 m-3">Services</router-link></li>
-        <li><router-link to="/" class="p-3 m-3">Contact</router-link></li>
+      <ul class="flex flex-row justify-center items-center m-3">
+        <li>
+          <router-link :to="{ name: 'home' }" class="p-3 m-3">Home</router-link>
+        </li>
+        <li>
+          <router-link :to="{ name: 'about' }" class="p-3 m-3"
+            >About Us</router-link
+          >
+        </li>
+        <li>
+          <router-link :to="{ name: 'services' }" class="p-3 m-3"
+            >Services</router-link
+          >
+        </li>
+        <li>
+          <router-link :to="{ name: 'contact' }" class="p-3 m-3"
+            >Contact</router-link
+          >
+        </li>
       </ul>
+    </div>
+    <h3>Get in touch</h3>
+    <div class="flex flex-row justify-center align-center">
+      <div class="flex flex-column m-3 justify-center align-center">
+        <p>Email: Email@email.com</p>
+      </div>
+      <div class="flex flex-column m-3 justify-center align-center">
+        <p>Phone: (+xxx) 1234-5678-9876</p>
+      </div>
     </div>
   </footer>
 </template>
