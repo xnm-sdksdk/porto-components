0b2a3b83eb0872e26dc14feb31bde705f2ddece9
xnm-sdksdk <n.mendonca@protonmail.com>
2024-01-24 15:59:43 +0000

    config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/your-production-path/" : "/",
   transpileDependencies: true,
 });
