27c823ff86c5c733eee1f0e65f91c5584a580fb6
xnm-sdksdk <n.mendonca@protonmail.com>
2024-01-25 15:19:34 +0000

    redesign

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,12 +5,13 @@
       <v-main>
         <div class="container mx-auto flex justify-between items-center p-4">
           <v-row class="justify-center">
-            <v-col :cols="12" md="5" lg="5">
+            <v-col :cols="10">
               <!-- First Card -->
-              <v-card>
-                <v-card-title class="headline">
+              <v-card class="rounded-xl bg-red-lighten-1">
+                <v-card-title class="text-h4">
                   Welcome to Porto Components App
                 </v-card-title>
+                <v-divider class="m-3"></v-divider>
                 <v-card-subtitle
                   >Built with Vuetify, Tailwind and Vue!</v-card-subtitle
                 >
@@ -30,47 +31,31 @@
         <!-- Second Card -->
         <div class="container mx-auto flex justify-between items-center p-4">
           <v-row class="justify-center">
-            <v-col :cols="12" md="8">
-              <v-card
-                class="mx-auto"
-                color="#1a1a1a"
-                theme="dark"
-                max-width="400"
-                prepend-icon="mdi-twitter"
-                title="X"
-              >
-                <template v-slot:prepend>
-                  <v-icon size="x-large"></v-icon>
-                </template>
-
-                <v-card-text class="text-h5 py-2">
-                  "When something is important enough, you do it even if the
-                  odds are not in your favor."
+            <v-col :cols="5">
+              <v-card class="rounded-xl bg-red-lighten-1">
+                <v-card-title class="text-h5">Web Development</v-card-title>
+                <v-card-text class="text-left">
+                  Our expert team crafts stunning and responsive websites
+                  tailored to meet your business goals. Whether you need a
+                  simple landing page or a complex e-commerce platform, we
+                  deliver cutting-edge web solutions.
                 </v-card-text>
-
-                <v-card-actions>
-                  <v-list-item class="w-100">
-                    <template v-slot:prepend>
-                      <v-avatar
-                        color="grey-darken-3"
-                        image="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-                      ></v-avatar>
-                    </template>
-
-                    <v-list-item-title>Elon Musk</v-list-item-title>
-
-                    <v-list-item-subtitle>Tesla Creator</v-list-item-subtitle>
-
-                    <template v-slot:append>
-                      <div class="justify-self-end">
-                        <v-icon class="me-1" icon="mdi-heart"></v-icon>
-                        <span class="subheading me-2">256k</span>
-                        <span class="me-1">·</span>
-                        <v-icon class="me-1" icon="mdi-share-variant"></v-icon>
-                        <span class="subheading">45k</span>
-                      </div>
-                    </template>
-                  </v-list-item>
+                <v-card-actions class="flex justify-end m-2">
+                  <v-btn class="bg-white"> Learn More </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+            <v-col :cols="5">
+              <v-card class="rounded-xl bg-red-lighten-1">
+                <v-card-title class="text-h5">Software Solutions</v-card-title>
+                <v-card-text class="text-left">
+                  Transform your business with our custom software solutions.
+                  From user-friendly applications to enterprise-grade systems,
+                  we develop software that enhances efficiency, productivity,
+                  and user satisfaction.
+                </v-card-text>
+                <v-card-actions class="flex justify-end m-2">
+                  <v-btn class="bg-white"> Explore Solutions </v-btn>
                 </v-card-actions>
               </v-card>
             </v-col>
@@ -79,43 +64,22 @@
 
         <!-- Third Card -->
         <div class="container mx-auto flex justify-between items-center p-4">
-          <v-card class="mx-auto" max-width="344">
-            <v-card-text>
-              <div>Word of the Day</div>
-              <p class="text-h4 text--primary">be•nev•o•lent</p>
-              <p>adjective</p>
-              <div class="text--primary">
-                well meaning and kindly.<br />
-                "a benevolent smile"
-              </div>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn variant="text" color="deep-purple-accent-4">
-                Learn More
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </div>
-
-        <!-- Fourth Card -->
-        <div class="container mx-auto flex justify-between items-center p-4">
-          <v-card class="mx-auto" max-width="344">
-            <v-card-text>
-              <div>Word of the Day</div>
-              <p class="text-h4 text--primary">el•ee•mos•y•nar•y</p>
-              <p>adjective</p>
-              <div class="text--primary">
-                relating to or dependent on charity; charitable; charitable
-                donations. Pertaining to alms.<br />
-                "an eleemosynary educational institution." "a benevolent smile"
-              </div>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn variant="text" color="deep-purple-accent-4">
-                Learn More
-              </v-btn>
-            </v-card-actions>
-          </v-card>
+          <v-row class="justify-center">
+            <v-col :cols="10">
+              <v-card class="rounded-xl bg-red-lighten-1">
+                <v-card-title class="text-h5"> Cloud Computing </v-card-title>
+                <v-card-text class="text-left">
+                  Harness the power of the cloud with our scalable and secure
+                  solutions. Whether you're looking to migrate to the cloud,
+                  optimize your infrastructure, or implement cloud-native
+                  applications, we've got you covered.
+                </v-card-text>
+                <v-card-actions class="flex justify-end m-2">
+                  <v-btn class="bg-white"> Cloud Services </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
         </div>
       </v-main>
     </v-container>
